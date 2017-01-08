c11c169a8cd7111f1f55f148574715f0bf227219
Issa Rice <riceissa@gmail.com>
2017-01-07 23:24:39 -0800

    add

---
+++ sql/donors.sql
@@ -0,0 +1,20 @@
+create table donors (
+    donor_id int(11) not null auto_increment primary key,
+    donor varchar(40) not null,
+    facebook_username varchar(100),
+    website varchar(40),
+    notes varchar(2000) default null,
+    twitter_username varchar(40),
+    lesswrong_username varchar(40),
+    eaf_username varchar(40),
+    gratipay_username varchar(40),
+    patreon_username varchar(40),
+    predictionbook_username varchar(40),
+    github_username varchar(40),
+    unique key donor(`donor`)
+) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into donors(donor) values
+    ('Jeff Kaufman');
+
+update donors set website='http://jefftk.com/' where donor = 'Jeff Kaufman';
