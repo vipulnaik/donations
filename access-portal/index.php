90cf4679f3d1d14e6cb1e534897410c1c30a6f0d
Vipul Naik <vipulnaik1@gmail.com>
2017-01-08 22:32:47 +0000

    Added first round of files to provide basic landing portal

---
+++ access-portal/index.php
@@ -0,0 +1,11 @@
+<?php
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+print '<title>Donations recorded by Vipul Naik</title>';
+include_once('analytics.inc');
+print '</head>';
+include_once("backend/globalVariables/passwordFile.inc");
+print '<body>';
+print '<h3>Donations recorded for Vipul Naik</h3>';
+include_once('preamble.inc');
+print '</body>';
+?>
