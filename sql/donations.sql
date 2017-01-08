02a4617bcb9c8f9a4cc05a3857346c546c98d75e
Issa Rice <riceissa@gmail.com>
2017-01-08 01:23:37 -0800

    Add one donation

--- sql/donations.sql
+++ sql/donations.sql
@@ -12,3 +12,9 @@ create table donations(
     matched boolean,
     goal_amount float(14,2),
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert into donations(donor, donee, amount, fraction, donation_date,
+    cause_area, url, notes, payment_modality, matched, goal_amount) values
+    ('Vipul Naik', 'GiveDirectly', 500.00, NULL, '2012-12', 'Global poverty',
+    NULL, NULL, NULL, NULL, NULL)
+;
