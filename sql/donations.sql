832bd1c155851fbe5119165c4b0b58d084921e1c
Vipul Naik <vipulnaik1@gmail.com>
2017-01-09 07:33:07 +0000

    Updated Ben Kuhn donations; collapsed donation matches and added link to post explaining 2016 giving

--- sql/donations.sql
+++ sql/donations.sql
@@ -306,16 +306,14 @@ insert into donations(donor, donee, amount, fraction, donation_date, donation_da
     ('Ben Kuhn', 'GiveDirectly', 120.00, NULL, '2013-07-06', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
     ('Ben Kuhn', 'Schistosomiasis Control Initiative', 80.00, NULL, '2013-07-06', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
     ('Ben Kuhn', 'Against Malaria Foundation', 200.00, NULL, '2013-07-06', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell (unrestricted)', 6000.00, NULL, '2013-11-22', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell (unrestricted)', 10000.00, NULL, '2014-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell (unrestricted)', 10000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 6000.00, NULL, '2013-11-22', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 10000.00, NULL, '2014-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 10000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, 'GiveWell', 10000, 'Wave'),
     ('Ben Kuhn', 'EA Policy Analytics', 6333.00, NULL, '2015-06-07', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell (unrestricted)', 10000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
     ('Ben Kuhn', 'Charity Entrepreneurship', 2000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'EA Giving Group', 10000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
     ('Ben Kuhn', 'GiveDirectly', 1000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell top charities', 5000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'EA Giving Group', 20000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    ('Ben Kuhn', 'GiveWell top charities', 5000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', 'See http://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'GiveWell', NULL, NULL),
+    ('Ben Kuhn', 'EA Giving Group', 20000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'http://www.benkuhn.net/ea/', 'See http://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'Nick Beckstead', 10000, 'Wave');
 
 insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis,
     cause_area, url, notes, payment_modality, match_eligible, goal_amount, influencer, employer_match, matching_employer)
