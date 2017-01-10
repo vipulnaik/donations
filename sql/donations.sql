bd5a689e627d2fd2a21253f56593e1cb00ef13e4
Vipul Naik <vipulnaik1@gmail.com>
2017-01-09 20:44:02 -0800

    Added Patrick donations

--- sql/donations.sql
+++ sql/donations.sql
@@ -16,7 +16,7 @@ create table donations (
     influencer varchar(1000),
     employer_match float(14,2),
     matching_employer varchar(100),
-    special_donation_reason enum('Giving Game','Compensation to charity for effort','Committed periodic donation','Fundraiser/Gift','Hat tip','One-off opportunity')
+    special_donation_reason enum('Giving Game','Compensation to charity for effort','Committed periodic donation','Fundraiser/Gift','Hat tip','One-off opportunity', 'Offset')
 ) ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis,
@@ -293,3 +293,29 @@ insert into donations(donor, donee, amount, fraction, donation_date,
     ('Pablo Stafforini', 'Animal Charity Evaluators', 1000.00, NULL, '2013-01-01', 'day', 'donation log', NULL, 'http://www.stafforini.com/blog/donations/', NULL, NULL, NULL, NULL, NULL, NULL, NULL)
 ;
 
+insert into donations(donor, donee, amount, fraction, donation_date,
+    donation_date_precision, donation_date_basis, cause_area, url, notes,
+    payment_modality, match_eligible, goal_amount, influencer, employer_match,
+    matching_employer, special_donation_reason)
+    values
+    ('Patrick Brinich-Langlois','EA Giving Group',10055.55,NULL,'2016-12-30','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/','Chose amount to reach donation goal total of 55555.55', NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','EA Giving Group',15000,NULL,'2016-11-05','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','EA Giving Group',10000,NULL,'2016-07-04','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','Mercy for Animals',500,NULL,'2016-06-13','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/','Offsetting a month of omnivore diet', NULL, NULL, 55555.55, NULL, NULL, 'Offset'),
+    ('Patrick Brinich-Langlois','EA Giving Group',10000,NULL,'2016-04-22','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','.impact',5000,NULL,'2016-03-21','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','.impact',5000,NULL,'2016-02-01','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, 55555.55, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','Centre for Effective Altruism',5000,NULL,'2015-12-22','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','Animal Charity Evaluators',10000,NULL,'2015-12-22','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','80,000 Hours',1692.42,NULL,'2015-10-06','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','80,000 Hours',38307.58,NULL,'2015-08-24','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/','Grant from The Eleemosynary Flying Orang−Utans Fund', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','Animal Charity Evaluators',10000,NULL,'2015-07-07','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/','Grant from The Eleemosynary Flying Orang−Utans Fund', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','80,000 Hours',30000,NULL,'2014-01-08','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','St. Olaf College',10,NULL,'2014-01-08','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinish-Langlois','Animal Charity Evaluators',10000,NULL,'2013-07-02','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','80,000 Hours',500,NULL,'2012-11-06','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinich-Langlois','St. Olaf College',2,NULL,'2012-10-31','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/','Comment: I was told that even a small amount would help immensely', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinish-Langlois','The Humane League',800,NULL,'2012-04-12','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinish-Langlois','The Humane League',800,NULL,'2012-04-23','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinish-Langlois','The Humane League',800,NULL,'2012-05-04','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Patrick Brinish-Langlois','The Humane League',800,NULL,'2012-05-30','day','donation log',NULL,'http://www.patbl.com/misc/other/donations/',NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
