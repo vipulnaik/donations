597f1f3c451eceaa46361365e6fb0d0c468cb634
Vipul Naik <vipulnaik1@gmail.com>
2017-04-08 16:57:48 +0000

    More cause area filters and hyperlinks for them

--- access-portal/index.php
+++ access-portal/index.php
@@ -3,6 +3,14 @@ print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><hea
 print '<title>Donations recorded by Vipul Naik</title>';
 include_once('analytics.inc');
 include_once('backend/stringFunctions.inc');
+$causeAreaFilterString = '';
+$causeAreaFilterStringHelper = '';
+$causeAreaFilterQueryComponent = "";
+if (!empty($_REQUEST['cause_area_filter'])) {
+  $causeAreaFilterString = $_REQUEST['cause_area_filter'];
+  $causeAreaFilterStringHelper = " (filtered to cause areas matching $causeAreaFilterStringHelper)";
+  $causeAreaFilterQueryComponent = " where cause_area REGEXP '$causeAreaFilterString' ";
+}
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
