a1701f59536f2cb953d8a84abce7e16527082f9d
Issa Rice <riceissa@gmail.com>
2017-07-15 23:46:08 -0700

    More playing around with deduplication

--- dedup-donees/dedup.py
+++ dedup-donees/dedup.py
@@ -1,17 +1,27 @@
 import re
 
 
-donees = []
+def donees(filename):
+    donees = []
 
-with open("temp", "r") as f:
-    for line in f:
-        d = line.strip().lower()
-        d = d.replace('.', '')
+    with open(filename, "r") as f:
+        for line in f:
+            d = line.strip().lower()
+            d = d.replace('.', '')
+            d = d.replace(' and ', '&')
+            d = d.replace('&', '')
 
-        d = re.sub(r",? inc\.?", "", d)
+            d = re.sub(r",? inc\.?", "", d).strip()
+            d = re.sub(r",? llc\.?", "", d).strip()
 
-        if d in donees:
-            print(d)
-        donees.append(d)
+            # if d in donees:
+            #     print(d)
+            donees.append(d)
+    return donees
 
-print(len(donees))
+# print(len(donees("temp")))
+print(len(set(donees("temp"))))
+# print(len(donees("temp2")))
+print(len(set(donees("temp2"))))
+
+print(set(donees("temp2")).difference(set(donees("temp"))))
