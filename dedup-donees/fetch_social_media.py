399d518b42e5ab0ab0fd866112d67010cf75a870
Issa Rice <riceissa@gmail.com>
2017-07-16 12:38:41 -0700

    Start on wikidata scrape

--- dedup-donees/fetch_social_media.py
+++ dedup-donees/fetch_social_media.py
@@ -6,7 +6,8 @@ import re
 
 
 def main():
-    print(get_homepage("Drugs for Neglected Diseases Initiative"))
+    # print(get_homepage("Drugs for Neglected Diseases Initiative"))
+    print(wikidata_official_website("Drugs for Neglected Diseases Initiative"))
 
 
 def get_homepage(orgname, lang="en"):
@@ -14,6 +15,31 @@ def get_homepage(orgname, lang="en"):
     Take orgname and try to return a homepage for that org.
     """
     return just_a_domain(orgname)
+
+
+def wikidata_official_website(orgname, lang="en"):
+    """
+    Apply the heuristic of "if Wikidata has an official website listed for an
+    org with this name, that is likely to be the official website".
+    """
+    payload = {
+            "action": "wbsearchentities",
+            "format": "json",
+            "language": lang,
+            "search": orgname,
+    }
+    r = requests.get('http://www.wikidata.org/w/api.php', params=payload)
+    result = r.json()
+    if 'error' in result:
+        logging.warning("FAILED %s %s", r.url, result['error'])
+    if 'warnings' in result:
+        logging.warning(result['warnings'])
+    candidate_org_ids = []
+    if "search" in result:
+        for d in result["search"]:
+            if "id" in d:
+                candidate_org_ids.append(d["id"])
+    return candidate_org_ids
 
 
 def just_a_domain(orgname, lang="en"):
