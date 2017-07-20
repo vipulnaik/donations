29cbc0d603d6461af3d58efa3caaa0f779c4a735
Issa Rice <riceissa@gmail.com>
2017-07-20 14:44:32 -0700

    Add initial jaccard similarity sql

---
+++ jaccard.sql
@@ -0,0 +1,21 @@
+-- Modified from https://stackoverflow.com/a/36823694/3422337
+select
+    jaccard.donor,
+    jaccard.intersect,
+    jaccard.union,
+    jaccard.intersect/jaccard.union as jaccard_index
+from (
+    select
+        d2.donor as donor,
+        count(dt2.donee) as 'intersect',
+        (select count(distinct dt3.donee) from
+         donations dt3 where dt3.donor in(d1.donor, d2.donor)) as 'union'
+    from
+        donors as d1
+        inner join donors as d2 on d1.donor != d2.donor
+        left join donations as dt1 on dt1.donor = d1.donor
+        left join donations as dt2 on dt2.donor = d2.donor and dt1.donee = dt2.donee
+    where d1.donor = 'Open Philanthropy Project'
+    group by d1.donor, d2.donor
+) as jaccard
+order by jaccard_index desc;
