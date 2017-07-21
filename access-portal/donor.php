308aaec614c3ba50c35de5b200f53962a38e66af
Issa Rice <riceissa@gmail.com>
2017-07-21 15:48:41 -0700

    Just call the general function for both kinds of graphs

--- access-portal/donor.php
+++ access-portal/donor.php
@@ -43,6 +43,7 @@ print '<li><a href="#donorDocumentList">Donor document list</a></li>';
 print '<li><a href="#donorDonationList">Donor donation list</a></li>';
 print '</ul>';
 
+include_once('backend/yearlyGraph.inc');
 include ("backend/donorInfo.inc");
 include ("backend/donorDonationAmountsByCauseAreaAndYear.inc");
 include ("backend/donorDonationAmountsByDoneeAndYear.inc");
