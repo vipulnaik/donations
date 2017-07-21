314f05126aa02cf69d4838005e0df926b184202f
Issa Rice <riceissa@gmail.com>
2017-07-20 23:35:53 -0700

    Add weighted_dot_product
    Thanks to Vipul

--- similarity.sql
+++ similarity.sql
@@ -10,8 +10,11 @@ select
     round(similarity.intersect/similarity.union, 4) as jaccard_index,
     round(similarity.intersect / (sqrt(similarity.donor_size) *
         sqrt(similarity.other_donor_size)), 4) as cosine_similarity,
-    similarity.weighted_magnitude,
-    similarity.weighted_magnitude_other
+    round(similarity.weighted_magnitude, 4) as weighted_magnitude,
+    round(similarity.weighted_magnitude_other, 4) as weighted_magnitude_other,
+    round(similarity.weighted_dot_product, 4) as weighted_dot_product,
+    round(similarity.weighted_dot_product / (similarity.weighted_magnitude *
+        similarity.weighted_magnitude_other), 4) as weighted_cosine_similarity
 from (
     select
         other_donors.donor as donor,
@@ -48,7 +51,18 @@ from (
             ) as sqsums
             where donor = other_donors.donor
         ) as `weighted_magnitude_other`,
-        (1) as `weighted_dot_product`
+        (select sum(sums1.s * sums2.s)
+            from (
+                select donor as donor1,donee,sum(amount) as s
+                from donations
+                group by donee, donor
+            ) as sums1 left join (
+                select donor as donor2,donee,sum(amount) as s
+                from donations
+                group by donee, donor
+            ) as sums2 on sums1.donee = sums2.donee
+            where donor1 = 'Open Philanthropy Project' and donor2 = other_donors.donor
+        ) as `weighted_dot_product`
     from (
         select distinct(donor) from donations
         where donor != 'Open Philanthropy Project'
