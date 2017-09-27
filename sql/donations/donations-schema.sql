cd57fbbe8411f384a3fc0b7130efc67c9cf1b941
Issa Rice <riceissa@gmail.com>
2017-09-27 16:19:36 -0700

    Add EA Survey in donation_date_basis

--- sql/donations/donations-schema.sql
+++ sql/donations/donations-schema.sql
@@ -6,7 +6,7 @@ create table donations (
     fraction float(3, 2),
     donation_date date,
     donation_date_precision enum('day','month','year','multi-year'),
-    donation_date_basis enum('transaction','donation log','date of donation announced in announcement','date of donation announcement','donee contributor list', 'IATI'),
+    donation_date_basis enum('transaction','donation log','date of donation announced in announcement','date of donation announcement','donee contributor list', 'IATI','Effective Altruism Survey'),
     cause_area varchar(100),
     url varchar(200),
     donor_cause_area_url varchar(200),
