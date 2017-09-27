2a8114ec106689853a4ab8cd875def65f136a438
Issa Rice <riceissa@gmail.com>
2017-09-27 16:14:56 -0700

    Remove extraneous column

--- sql/donors.sql
+++ sql/donors.sql
@@ -26,7 +26,7 @@ create table donors (
 insert into donors(donor, donor_type, country, facebook_username, website, donations_url, lesswrong_username, linkedin_username,affiliated_orgs,eaf_username, eahub_username,github_username,twitter_username, predictionbook_username) values
   ('Laura and John Arnold Foundation','Private foundation','United States','Laura-and-John-Arnold-Foundation-177757582300684','http://www.arnoldfoundation.org/','http://www.arnoldfoundation.org/grants/',NULL,NULL,NULL,NULL,NULL,NULL,'lja_foundation',NULL),
   ('Open Philanthropy Project','Private foundation','United States','openphilanthropy','http://www.openphilanthropy.org/','http://www.openphilanthropy.org/giving/grants',NULL,NULL,'GiveWell,Good Ventures',NULL,NULL,NULL,'open_phil','OpenPhilUnofficial'),
-  ('GiveWell Incubation Grants','Subsidiary','United States',NULL,'http://www.givewell.org/research/incubation-grants','http://www.givewell.org/research/incubation-grants',NULL,NULL,'GiveWell,Good Ventures',NULL,NULL,NULL,NULL,NULL,NULL),
+  ('GiveWell Incubation Grants','Subsidiary','United States',NULL,'http://www.givewell.org/research/incubation-grants','http://www.givewell.org/research/incubation-grants',NULL,NULL,'GiveWell,Good Ventures',NULL,NULL,NULL,NULL,NULL),
   ('Good Ventures/GiveWell top and standout charities','Subsidiary','United States',NULL,NULL,NULL,NULL,NULL,'GiveWell,Good Ventures,Open Philanthropy Project',NULL,NULL,NULL,NULL,NULL),
   ('Good Ventures/not recommended by GiveWell or Open Philanthropy Project','Subsidiary','United States',NULL,NULL,NULL,NULL,NULL,'Good Ventures',NULL,NULL,NULL,NULL,NULL),
   ('GiveWell discretionary regranting','Subsidiary','United States',NULL,NULL,NULL,NULL,NULL,'GiveWell',NULL,NULL,NULL,NULL,NULL),
