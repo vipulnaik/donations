7ef803ffabf4c1b2360541f25fbd30ff87964466
Issa Rice <riceissa@gmail.com>
2017-12-20 13:16:57 -0800

    add joey donations

---
+++ sql/donations/joey-savoie.sql
@@ -0,0 +1,7 @@
+insert into donations(donor, donee, amount, fraction, donation_date,
+    donation_date_precision, donation_date_basis, cause_area, url, notes,
+    payment_modality, match_eligible, goal_amount, influencer, employer_match,
+    matching_employer, amount_original_currency, original_currency) values
+    ('Joey Savoie','Against Malaria Foundation, Deworm the World, GiveDirectly, The Humane League, Schistosomiasis Control Initiative (SCI), Vegan Outreach, Effective fundraising',NULL,NULL,'2013-01-01','year','donation log','Global health and poverty','https://eahub.org/user/joey-savoie',NULL,NULL,NULL,NULL,NULL,NULL,NULL,6000,'CAD')
+    ,('Joey Savoie','Various',NULL,NULL,'2013-01-01','multi-year','donation log',NULL,'https://eahub.org/user/joey-savoie','Donations before 2013',NULL,NULL,NULL,NULL,NULL,NULL,19000,'CAD')
+;
