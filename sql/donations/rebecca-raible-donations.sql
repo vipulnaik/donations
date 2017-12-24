54d87f7b5413379a547ec841f975a6b37d8434bb
Vipul Naik <vipulnaik1@gmail.com>
2017-12-23 21:23:37 -0800

    Fix/update Rebecca Raible donations

--- sql/donations/rebecca-raible-donations.sql
+++ sql/donations/rebecca-raible-donations.sql
@@ -1,4 +1,10 @@
-insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes) values
-  ('Rebecca Raible', 'GiveWell top charities', NULL, 1.0, '2015-01-01','month','donation log','Global health and cash transfers','https://eahub.org/user/rebecca-raible','Old entry in EA Hub profile'),
-  ('Rebecca Raible', 'Donor lottery', 2000.00, NULL, '2016-12-12', 'day', 'transaction', NULL, 'http://effective-altruism.com/ea/14d/donor_lotteries_a_stepbystep_guide_for_mall/', NULL);
+insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes, influencer) values
+  # -- 2014
+  ('Rebecca Raible', 'GiveWell top charities', NULL, 1.0, '2015-01-01','month','donation log','Global health and cash transfers','https://eahub.org/user/rebecca-raible','Old entry in EA Hub profile. For reasoning, see https://blog.givewell.org/2014/12/17/staff-members-personal-donations-giving-season-2014/', 'GiveWell'),
+  # -- 2015
+  ('Rebecca Raible','GiveWell top charities',NULL,0.8,'2015-12-09','day','date of donation announcement','Global health and cash transfers','https://blog.givewell.org/2015/12/09/staff-members-personal-donations-for-giving-season-2015/','Although the GiveWell recommendation is to donate to the Against Malaria Foundation, donor believes that giving to GiveWell for regranting allows for more flexibility in the use of funds based on new information', 'GiveWell'),
+  ('Rebecca Raible','GiveDirectly',NULL,0.1,'2015-12-09','day','date of donation announcement','Global health and cash transfers','https://blog.givewell.org/2015/12/09/staff-members-personal-donations-for-giving-season-2015/','Donor thinks of GiveDirectly as an important tool helping donors evaluate the quality of other giving opportunities', 'GiveWell'),
+  # -- Remaining 10% to "other causes"
+  # -- 2016
+  ('Rebecca Raible', 'Donor lottery', 2000.00, NULL, '2016-12-12', 'day', 'transaction', NULL, 'http://effective-altruism.com/ea/14d/donor_lotteries_a_stepbystep_guide_for_mall/', 'For some musings prior to making the donation decision, see https://blog.givewell.org/2016/12/09/staff-members-personal-donations-giving-season-2016/', NULL);
 
