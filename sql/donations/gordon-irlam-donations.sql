c8deff9c1ea2340daf0ab16512ceee212ee76b52
Vipul Naik <vipulnaik1@gmail.com>
2017-12-27 07:55:07 -0800

    Start adding Gordon Irlam donations

---
+++ sql/donations/gordon-irlam-donations.sql
@@ -0,0 +1,24 @@
+insert into donations(donor, donee, amount, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes) values
+  # -- 2002 donations: http://www.gricf.org/2002-report.html
+  ('Gordon Irlam','Consumer Project on Technology', 1000, '2002-01-01', 'year', 'donation log', NULL, 'http://www.gricf.org/2002-report.html', 'c/o Essential Information'),
+  ('Gordon Irlam','Sustainable Sciences Institute', 1000,' 2002-01-01', 'year', 'donation log', NULL, 'http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Grantmakers Without Borders', 100, '2002-01-01', 'year', 'donation log', NULL, 'http://www.gricf.org/2002-report.html', 'c/o Tides'),
+  ('Gordon Irlam','Global Health Council', 250, '2002-01-01', 'year', 'donation log', 'Global health', 'http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Funders Concerned About AIDS', 100, '2002-01-01', 'year', 'donation log', 'Global health/STD control including HIV/AIDS','http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','AIDS Vaccine Advocacy Coalition', 250, '2002-01-01', 'year', 'donation log', 'Global health/STD control including HIV/AIDS','http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Bay Area International Development Organizations',100,'2002-01-01','year','donation log',NULL,'http://www.gricf.org/2002-report.html', 'c/o Tides'),
+  ('Gordon Irlam','San Francisco AIDS Foundation',100,'2002-01-01','year','donation log','Global health/STD control including HIV/AIDS','http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Grameen Foundation USA',100,'2002-01-01','year','donation log',NULL,'http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Global Exchange',100,'2002-01-01','year','donation log',NULL,'http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Oxfam America',100,'2002-01-01','year','donation log',NULL,'http://www.gricf.org/2002-report.html', NULL),
+  ('Gordon Irlam','Community Institute for Psychotherapy',100,'2002-01-01','year','donation log',NULL,'http://www.gricf.org/2002-report.html', NULL),
+  # -- 2003 donations: http://www.gricf.org/2003-report.html
+  ('Gordon Irlam','Sustainable Sciences Institute',1100,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html',NULL),
+  ('Gordon Irlam','HealthGAP Coalition',1000,'2003-01-01','year','donation log','Global health','http://www.gricf.org/2003-report.html','c/o Mobilization Against AIDS'),
+  ('Gordon Irlam','Institute for One World Health',1000,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html',NULL),
+  ('Gordon Irlam','Grameen Technology Center',1000,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html','c/o Grameen Foundation USA'),
+  ('Gordon Irlam','Global Health Council',250,'2003-01-01','year','donation log','Global health','http://www.gricf.org/2003-report.html',NULL),
+  ('Gordon Irlam',"Association for India's Development", # '
+   100,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html',NULL),
+   ('Gordon Irlam','African Immigrants Social and Cultural Services',100,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html',NULL),
+   ('Gordon Irlam','Microbicides as an Alternative Solution',100,'2003-01-01','year','donation log',NULL,'http://www.gricf.org/2003-report.html','c/o SFF-CIF'); # -- 2003 not yet complete
