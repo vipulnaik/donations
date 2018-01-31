42824c23ff8f10f5353c26d3c5a15475d0db0973
Issa Rice <riceissa@gmail.com>
2018-01-31 13:41:14 -0800

    Add Ford Foundation

--- sql/donors/private-foundations-and-subsidiaries.sql
+++ sql/donors/private-foundations-and-subsidiaries.sql
@@ -112,6 +112,8 @@ insert into donors(donor, donor_type, country, facebook_username, website, donat
 
   ('Hewlett Foundation','Private foundation','United States','HewlettFoundation','https://www.hewlett.org/','https://www.hewlett.org/grants/',NULL,'the-william-and-flora-hewlett-foundation',NULL,NULL,NULL,NULL,'hewlett_found',NULL,'https://www.hewlett.org/about-us/values-and-policies/','https://www.hewlett.org/grantmaking-faq/',NULL,NULL,NULL,NULL,'https://github.com/riceissa/hewlett-foundation','https://en.wikipedia.org/wiki/Hewlett_Foundation',NULL,'https://www.hewlett.org/about-us/hewlett-family-and-history/',NULL,NULL,NULL,NULL,NULL),
 
+  ('Ford Foundation','Private foundation','United States','FordFoundation','https://www.fordfoundation.org','https://www.fordfoundation.org/work/our-grants/',NULL,'ford-foundation',NULL,NULL,NULL,NULL,'FordFoundation',NULL,'https://www.fordfoundation.org/about-us/mission/','https://www.fordfoundation.org/work/our-grants/idea-submission/',NULL,NULL,NULL,NULL,'https://github.com/riceissa/ford-foundation','https://en.wikipedia.org/wiki/Ford_Foundation',NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+
   ('Pineapple Fund',NULL,'United States',NULL,'https://pineapplefund.org/','https://pineapplefund.org/',NULL,NULL,NULL,NULL,NULL,'PineappleFund',NULL,NULL,NULL,NULL,'irregular',NULL,'days','months','https://github.com/riceissa/pineapple-fund','https://en.wikipedia.org/wiki/Pineapple_Fund',NULL,
   'The Pineapple Fund was announced in December 2017 by an anonymous individual.',
   NULL, NULL, NULL, NULL, NULL);
