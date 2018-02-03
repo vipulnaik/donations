19d59a5a7ebcd2ed4e4ff64dc69a4efd3ba2adf0
Issa Rice <riceissa@gmail.com>
2018-02-03 15:14:35 -0800

    Add caching

--- access-portal/influencer.php
+++ access-portal/influencer.php
@@ -8,6 +8,7 @@ print "<title>$influencer money moved</title>";
 include_once('analytics.inc');
 include_once('strip-commas.inc');
 include_once('backend/stringFunctions.inc');
+include_once('backend/cachingFunctions.inc');
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
@@ -29,28 +30,35 @@ print '<li><a href="#influencerDocumentList">Influencer document list</a></li>';
 print '<li><a href="#influencerMoneyMovedList">Influencer money moved list</a></li>';
 print '</ul>';
 
-if ($stmt = $mysqli->prepare("select donor from donations where donor = ?")) {
-  $stmt->bind_param("s", $influencer);
-  $stmt->execute();
-  $result = $stmt->get_result();
-  if ($result->num_rows > 0) {
-    print '<p><a href="/donor.php?donor=' . urlencode($influencer)
-      . '">This entity is also a donor<a/>.</p>' . "\n";
+$cache_location = "cache/" . md5($_SERVER['REQUEST_URI']) . ".html";
+if (needToRegenerate($cache_location)) {
+  ob_start();
+  if ($stmt = $mysqli->prepare("select donor from donations where donor = ?")) {
+    $stmt->bind_param("s", $influencer);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+      print '<p><a href="/donor.php?donor=' . urlencode($influencer)
+        . '">This entity is also a donor<a/>.</p>' . "\n";
+    }
   }
-}
 
-if ($stmt = $mysqli->prepare("select donee from donations where donee = ?")) {
-  $stmt->bind_param("s", $influencer);
-  $stmt->execute();
-  $result = $stmt->get_result();
-  if ($result->num_rows > 0) {
-    print '<p><a href="/donee.php?donee=' . urlencode($influencer)
-      . '">This entity is also a donee<a/>.</p>' . "\n";
+  if ($stmt = $mysqli->prepare("select donee from donations where donee = ?")) {
+    $stmt->bind_param("s", $influencer);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if ($result->num_rows > 0) {
+      print '<p><a href="/donee.php?donee=' . urlencode($influencer)
+        . '">This entity is also a donee<a/>.</p>' . "\n";
+    }
   }
-}
 
-include ("backend/influencerDocumentList.inc");
-include ("backend/influencerMoneyMovedList.inc");
+  include ("backend/influencerDocumentList.inc");
+  include ("backend/influencerMoneyMovedList.inc");
+  $output = ob_get_clean();
+  file_put_contents($cache_location, $output);
+}
+include($cache_location);
 include_once("anchorjs.inc");
 print '</body>';
 ?>
