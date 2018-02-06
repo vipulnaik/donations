4bb4ed566f6d7570f47d56b5e59f91442220b41c
Issa Rice <riceissa@gmail.com>
2018-02-06 11:43:22 -0800

    add back the procedure part

--- similarity/similarity-incremental-donee.sql
+++ similarity/similarity-incremental-donee.sql
@@ -1,90 +1,82 @@
-# THIS IS A HACK.
-
 drop procedure if exists updateSimilarityDonee;
 
-drop table if exists donee_donor_pairs;
-drop table if exists donee_donor_pairs_2;
-drop table if exists two_donees_one_donor;
-drop table if exists donee_summary;
-drop table if exists donee_summary_2;
-drop table if exists donee_sim_pre;
-drop table if exists donee_sim;
-drop table if exists donee_similarity;
+delimiter $$
 
-/* delimiter $$ */
-
-set @dne = "Machine Intelligence Research Institute" COLLATE utf8_unicode_ci;
-
-/* create procedure updateSimilarityDonee(@dne varchar(100)) */
-/* begin */
-    /* delete from donee_donor_pairs where donee=@dne; */
-    create table donee_donor_pairs as
-    select
-        donee,
+create procedure updateSimilarityDonee(dne varchar(100))
+begin
+    delete from two_donees_one_donor where first_donee=dne or second_donee=dne;
+    insert into two_donees_one_donor (
+        first_donee,
+        second_donee,
         donor,
-        coalesce(sum(amount), 0) as total_donation,
-        count(*) as num_donations
-    from donations where donee=@dne group by donee, donor;
-
-    /* delete from donee_donor_pairs_2 where donee=@dne; */
-    create table donee_donor_pairs_2 as
+        first_donee_total,
+        second_donee_total,
+        first_num_donations,
+        second_num_donations
+    )
     select
-        donee,
-        donor,
-        coalesce(sum(amount), 0) as total_donation,
-        count(*) as num_donations
-    from donations where donee=@dne group by donee, donor;
-
-    /* delete from two_donees_one_donor where first_donee=@dne or second_donee=@dne; */
-    create table two_donees_one_donor as
-    select
-        donee_donor_pairs.donee as first_donee,
-        donee_donor_pairs_2.donee as second_donee,
-        donee_donor_pairs.donor as donor,
-        donee_donor_pairs.total_donation as first_donee_total,
-        donee_donor_pairs_2.total_donation as second_donee_total,
-        donee_donor_pairs.num_donations as first_num_donations,
-        donee_donor_pairs_2.num_donations as second_num_donations
-    from donee_donor_pairs
-    left join donee_donor_pairs_2 on
-        donee_donor_pairs.donor = donee_donor_pairs_2.donor
+        donor_donee_pairs.donee as first_donee,
+        donor_donee_pairs_2.donee as second_donee,
+        donor_donee_pairs.donor as donor,
+        donor_donee_pairs.total_donation as first_donee_total,
+        donor_donee_pairs_2.total_donation as second_donee_total,
+        donor_donee_pairs.num_donations as first_num_donations,
+        donor_donee_pairs_2.num_donations as second_num_donations
+    from donor_donee_pairs
+    left join donor_donee_pairs_2 on
+        donor_donee_pairs.donor = donor_donee_pairs_2.donor
         # yes, we already have the following condition in the "where" clause
         # below, but having it here in addition seems to make the
         # procedure run faster
-        and (donee_donor_pairs.donee=@dne or donee_donor_pairs_2.donee=@dne)
-    where donee_donor_pairs.donee=@dne or donee_donor_pairs_2.donee=@dne;
+        and (donor_donee_pairs.donee=dne or donor_donee_pairs_2.donee=dne)
+    where donor_donee_pairs.donee=dne or donor_donee_pairs_2.donee=dne;
 
-    /* delete from donee_summary where donee=@dne; */
-    create table donee_summary as
+    delete from donee_summary where donee=dne;
+    insert into donee_summary (donee, donor_count, weighted_magnitude)
     select
         donee,
         count(distinct donor) as donor_count,
         sqrt(sum(power(total_donation, 2))) as weighted_magnitude
-    from donee_donor_pairs where donee=@dne group by donee;
+    from donor_donee_pairs where donee=dne group by donee;
 
-    /* delete from donee_summary_2 where donee=@dne; */
-    create table donee_summary_2 as
+    delete from donee_summary_2 where donee=dne;
+    insert into donee_summary_2 (donee, donor_count, weighted_magnitude)
     select
         donee,
         count(distinct donor) as donor_count,
         sqrt(sum(power(total_donation, 2))) as weighted_magnitude
-    from donee_donor_pairs where donee=@dne group by donee;
+    from donor_donee_pairs where donee=dne group by donee;
 
-    /* delete from sim_pre where first_donee=@dne or second_donee=@dne; */
-    create table donee_sim_pre as
+    delete from donee_sim_pre where first_donee=dne or second_donee=dne;
+    insert into donee_sim_pre (
+        first_donee,
+        second_donee,
+        union_size,
+        weighted_dot_product
+    )
     select
         first_donee,
         second_donee,
-        (select count(distinct donor) from donee_donor_pairs
+        (select count(distinct donor) from donor_donee_pairs
          where donee in (first_donee, second_donee)
         ) as `union_size`,
         sum(first_donee_total * second_donee_total) as weighted_dot_product
     from two_donees_one_donor
-    where first_donee=@dne or second_donee=@dne
+    where first_donee=dne or second_donee=dne
     group by first_donee, second_donee;
 
-    /* delete from sim where first_donee=@dne or second_donee=@dne; */
-    create table donee_sim as
+    delete from donee_sim where first_donee=dne or second_donee=dne;
+    insert into donee_sim (
+        first_donee,
+        second_donee,
+        union_size,
+        intersect_size,
+        weighted_dot_product,
+        first_donee_size,
+        weighted_magnitude,
+        second_donee_size,
+        weighted_magnitude_other
+    )
     select
         first_donee,
         second_donee,
@@ -102,10 +94,20 @@ set @dne = "Machine Intelligence Research Institute" COLLATE utf8_unicode_ci;
             donee_sim_pre.first_donee = donee_summary.donee
         left join donee_summary_2 on
             donee_sim_pre.second_donee = donee_summary_2.donee
-    where first_donee=@dne or second_donee=@dne;
+    where first_donee=dne or second_donee=dne;
 
-    /* delete from similarity where first_donee=@dne or second_donee=@dne; */
-    create table donee_similarity as
+    delete from donee_similarity where first_donee=dne or second_donee=dne;
+    insert into donee_similarity (
+        first_donee,
+        second_donee,
+        first_donee_size,
+        second_donee_size,
+        union_size,
+        intersect_size,
+        jaccard_index,
+        cosine_similarity,
+        weighted_cosine_similarity
+    )
     select
         first_donee,
         second_donee,
@@ -121,9 +123,9 @@ set @dne = "Machine Intelligence Research Institute" COLLATE utf8_unicode_ci;
            round(weighted_dot_product / (weighted_magnitude *
               weighted_magnitude_other), 4)) as weighted_cosine_similarity
     from donee_sim where intersect_size > 0 and first_donee != second_donee
-        and (first_donee=@dne or second_donee=@dne);
+        and (first_donee=dne or second_donee=dne);
 
-/* end$$ */
-/* delimiter ; */
+end$$
+delimiter ;
 
 /* call updateSimilarityDonee("Machine Intelligence Research Institute"); */
