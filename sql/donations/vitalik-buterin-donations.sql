5aa491d11fd617b657465126a06f0d3354bc6046
Issa Rice <riceissa@gmail.com>
2018-04-03 14:32:10 -0700

    Add Vitalik Buterin donations
    Part of #50.

---
+++ sql/donations/vitalik-buterin-donations.sql
@@ -0,0 +1,8 @@
+# This file is manually edited. Vitalik's donation to MIRI is covered in the
+# MIRI top donors file so not listed here.
+
+insert into donations(donor, donee, amount, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes, payment_modality, match_eligible, employer_match, matching_employer) values
+    ('Vitalik Buterin','Internet Archive',93469.0,'2018-02-10','day','transaction','Digital preservation','https://www.ccn.com/intenet-archive-sees-big-donations-vitalik-buterin-pineapple-fund/',NULL,'Ethereum transaction',NULL,NULL,NULL)
+    ,('Vitalik Buterin','SENS Research Foundation',2400000.0,'2018-02-02','day','date of donation announcement','Life sciences/anti-aging','http://www.sens.org/outreach/press-releases/srf-receives-24m-ethereum-donation-from-vitalik-buterin',NULL,'Ethereum transaction',NULL,NULL,NULL)
+    ,('Vitalik Buterin','GiveDirectly',1000000.0,'2018-03-27','day','date of donation announcement','Global poverty','https://givedirectly.org/blog-post?id=1228348992037272486','OmiseGO also contributes to this donation','OMG transaction',NULL,NULL,NULL)
+;
