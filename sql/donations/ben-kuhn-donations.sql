2cedc75ec41203fcfe7ef3a77af4b7a0f9d2cd93
Vipul Naik <vipulnaik1@gmail.com>
2018-04-06 21:58:10 -0700

    Update Ben Kuhn to add 2017 donations

--- sql/donations/ben-kuhn-donations.sql
+++ sql/donations/ben-kuhn-donations.sql
@@ -1,14 +1,20 @@
 insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis,
     cause_area, url, notes, payment_modality, match_eligible, goal_amount, influencer, employer_match, matching_employer)
     values
-    ('Ben Kuhn', 'GiveDirectly', 120.00, NULL, '2013-07-06', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'Schistosomiasis Control Initiative', 80.00, NULL, '2013-07-06', 'day', 'donation log', 'Global health/deworming', 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'Against Malaria Foundation', 200.00, NULL, '2013-07-06', 'day', 'donation log', 'Global health/malaria', 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell', 6000.00, NULL, '2013-11-22', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell', 10000.00, NULL, '2014-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell', 10000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, 'GiveWell', 10000, 'Wave'),
-    ('Ben Kuhn', 'Effective Altruism Policy Analytics', 6333.00, NULL, '2015-06-07', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/eapa for more context', NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'Charity Entrepreneurship', 2000.00, NULL, '2015-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveDirectly', 1000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    ('Ben Kuhn', 'GiveWell top charities', 5000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'GiveWell', NULL, NULL),
-    ('Ben Kuhn', 'EA Giving Group', 20000.00, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'Nick Beckstead', 10000, 'Wave');
+    ('Ben Kuhn', 'GiveDirectly', 120, NULL, '2013-07-06', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'Schistosomiasis Control Initiative', 80, NULL, '2013-07-06', 'day', 'donation log', 'Global health/deworming', 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'Against Malaria Foundation', 200, NULL, '2013-07-06', 'day', 'donation log', 'Global health/malaria', 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 6000, NULL, '2013-11-22', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 10000, NULL, '2014-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell', 10000, NULL, '2015-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, 'GiveWell', 10000, 'Wave'),
+    ('Ben Kuhn', 'Effective Altruism Policy Analytics', 6333, NULL, '2015-06-07', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/eapa for more context', NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'Charity Entrepreneurship', 2000, NULL, '2015-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    # -- 2016 donations
+    ('Ben Kuhn', 'GiveDirectly', 1000, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn', 'GiveWell top charities', 5000, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'GiveWell', NULL, NULL),
+    ('Ben Kuhn', 'EA Giving Group', 20000, NULL, '2016-12-31', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2016 for more context', NULL, NULL, NULL, 'Nick Beckstead', 10000, 'Wave'),
+    # -- 2017 donations
+    ('Ben Kuhn', 'GiveWell', 1400, NULL, '2017-12-30', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2017 for more context', NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn','GiveWell top charities',12600, NULL, '2017-12-30', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2017 for more context', NULL, NULL, NULL, NULL, NULL, NULL),
+    ('Ben Kuhn','Ben Kuhn donor-advised fund',46000, NULL, '2017-12-28', 'day', 'donation log', NULL, 'https://www.benkuhn.net/ea/', 'See https://www.benkuhn.net/giving-2017 for more context', NULL, NULL, NULL, NULL, 10000, 'Wave');
+    
