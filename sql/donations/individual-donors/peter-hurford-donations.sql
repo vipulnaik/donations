1a21138450889de6149cefe693679b6054975774
Vipul Naik <vipulnaik1@gmail.com>
2018-05-28 10:02:59 -0700

    Add Ellie Raison donation via Peter Hurford

--- sql/donations/individual-donors/peter-hurford-donations.sql
+++ sql/donations/individual-donors/peter-hurford-donations.sql
@@ -1,3 +1,7 @@
+# -- Donations made by others via Peter
+insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes, special_donation_reason, affected_countries) values
+  ('Ellie Raison','No Means No Worldwide',6986,NULL,'2018-05-18','day','donation log','Sexual violence','http://peterhurford.com/other/donations.html','Money sent via Peter Hurford to be eligible for tax deduction benefits','Giving Game',NULL);
+
 # -- Contract work "donations"
 
 insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes, special_donation_reason, affected_countries, is_contractwork) values
