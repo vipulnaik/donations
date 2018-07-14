d480109806f76715e217af0638d27d9ef1a78e0a
Issa Rice <riceissa@gmail.com>
2018-07-14 08:58:21 -0700

    Fix orgwatch query

--- sql/donors/aiwatch-update.sql
+++ sql/donors/aiwatch-update.sql
@@ -1,4 +1,4 @@
-# -- This will only work if you have already got the aiwatch database on the same MySQL server, with name `aiwatch`. Get the database from https://github.com/riceissa/aiwatch
-update donors set aiwatch = true where donor in (select distinct person from aiwatch.people) and donor_type = 'Individual';
-update donors set aiwatch = true where donor in (select distinct organization from aiwatch.organizations) and donor_type in ('Private foundation','Subsidiary');
+# -- This will only work if you have already got the orgwatch/aiwatch database on the same MySQL server, with name `aiwatch`. Get the database from https://github.com/riceissa/aiwatch
+update donors set orgwatch = true where donor in (select distinct person from aiwatch.people) and donor_type = 'Individual';
+update donors set orgwatch = true where donor in (select distinct organization from aiwatch.organizations) and donor_type in ('Private foundation','Subsidiary');
 
