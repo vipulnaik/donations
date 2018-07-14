f2b0cfcf8ba961044f374509d185e25df098fa38
Issa Rice <riceissa@gmail.com>
2018-07-14 08:55:08 -0700

    Fix orgwatch query

--- sql/donees/aiwatch-update.sql
+++ sql/donees/aiwatch-update.sql
@@ -1,2 +1,2 @@
-# -- This will only work if you have already got the aiwatch database on the same MySQL server, with name `aiwatch`. Get the database from https://github.com/riceissa/aiwatch
-update donees set aiwatch = true where donee in (select distinct organization from aiwatch.organizations);
+# -- This will only work if you have already got the orgwatch/aiwatch database on the same MySQL server, with name `aiwatch`. Get the database from https://github.com/riceissa/aiwatch
+update donees set orgwatch = true where donee in (select distinct organization from aiwatch.organizations);
