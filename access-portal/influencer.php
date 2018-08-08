0dab82663d3926312f969070d033b1988a5218f3
Issa Rice <riceissa@gmail.com>
2018-08-08 14:42:46 -0700

    Include yearly graph file
    Part of #69.

--- access-portal/influencer.php
+++ access-portal/influencer.php
@@ -9,6 +9,7 @@ include_once('analytics.inc');
 include_once('strip-commas.inc');
 include_once('backend/stringFunctions.inc');
 include_once('backend/cachingFunctions.inc');
+include_once('backend/yearlyGraph.inc');
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
