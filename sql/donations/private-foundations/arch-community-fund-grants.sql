b1f8c8ad9c4116d064beeefe393bb73612b73561
Issa Rice <riceissa@gmail.com>
2018-12-27 16:55:51 -0800

    Add Arch Community Fund grants
    Closes #87.

---
+++ sql/donations/private-foundations/arch-community-fund-grants.sql
@@ -0,0 +1,12 @@
+insert into donations (donor, donee, amount, donation_date,
+    donation_date_precision, donation_date_basis, cause_area, url,
+    donor_cause_area_url, notes) values
+  ('Arch Community Fund', 'Bay Resistance', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Black Alliance for Just Immigration', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Center for Media Justice', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Colectiva Legal del Pueblo', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Critical Resistance', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'National Lawyers Guild (SF Bay Area Chapter)', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Rural Organizing Project', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+  ,('Arch Community Fund', 'Unite Oregon', 25000, '2018-05-16', 'day', 'date of donation announcement', 'Grassroots activism', 'http://www.archcommunityfund.org/grantee/2018/05/16/test-grantee.html', NULL, NULL)
+;
