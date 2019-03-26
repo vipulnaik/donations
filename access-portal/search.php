610f4df90e1fb99d684f3a8fc9b90a1974731fae
Issa Rice <riceissa@gmail.com>
2019-03-25 17:42:08 -0700

    Add new keyword search page
    For #110.

---
+++ access-portal/search.php
@@ -0,0 +1,38 @@
+<?php
+include_once('doctype.inc');
+$keyword = "Giving Tuesday";
+if (($_REQUEST['keyword'] ?? '') != '') {
+  $keyword = $_REQUEST['keyword'];
+}
+
+print "<title>All donations matching “" . htmlspecialchars($keyword) . "”</title>\n";
+include_once('analytics.inc');
+include_once('strip-commas.inc');
+include_once('backend/stringFunctions.inc');
+include_once('backend/cachingFunctions.inc');
+print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
+print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
+print '</head>';
+include_once("backend/globalVariables/passwordFile.inc");
+include_once("backend/globalVariables/lists.inc");
+print '<body>';
+print '<script>$(document).ready(function()
+    {
+        $("#myTableSearchDonationList").tablesorter({textExtraction: stripCommas});
+        $("#myTableSearchDocumentList").tablesorter({textExtraction: stripCommas});
+    }
+); </script>'."\n";
+print "<h3>All donations matching “" . htmlspecialchars($keyword) . "”</h3>\n";
+include_once('preamble.inc');
+print '<h4>Table of contents</h4>';
+print '<ul>';
+print '<li><a href="#searchDonationList">List of donations</a></li>';
+print '<li><a href="#searchDocumentList">List of documents</a></li>';
+print '</ul>';
+
+include ("backend/searchDonationList.inc");
+include ("backend/searchDocumentList.inc");
+
+include_once('anchorjs.inc');
+print '</body>';
