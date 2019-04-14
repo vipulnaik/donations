42ba7309e2db06f6eea5ca0c26bb492f48cc7a24
Issa Rice <riceissa@gmail.com>
2019-04-13 17:02:01 -0700

    Add Quixote Foundation metadata
    For #97.

--- sql/donors/private-foundations-and-subsidiaries.sql
+++ sql/donors/private-foundations-and-subsidiaries.sql
@@ -208,6 +208,38 @@ insert into donors(donor, donor_type, country, facebook_username, website, donat
     NULL /* notes */
   ),
 
+  (
+    'Quixote Foundation', /* donor */
+    'Private foundation', /* donor_type */
+    'United States', /* country */
+    'QuixoteFoundation', /* facebook_username */
+    'http://quixotefoundation.org', /* website */
+    'http://quixotefoundation.org/grants/', /* donations_url */
+    NULL, /* lesswrong_username */
+    NULL, /* linkedin_username */
+    NULL, /* affiliated_orgs */
+    NULL, /* eaf_username */
+    NULL, /* eahub_username */
+    NULL, /* github_username */
+    'QuixoteTilts', /* twitter_username */
+    NULL, /* predictionbook_username */
+    NULL, /* philosophy_url */
+    NULL, /* grant_application_process_url */
+    NULL, /* donations_data_update_regularity */
+    NULL, /* donations_data_dlw_update_regularity */
+    NULL, /* donations_data_update_lag */
+    NULL, /* donations_data_dlw_update_lag */
+    'https://github.com/riceissa/quixote-foundation', /* dlw_data_processing_script */
+    NULL, /* wikipedia_page */
+    NULL, /* best_overview_url */
+    NULL, /* brief_history */
+    NULL, /* brief_donor_focus_notes */
+    NULL, /* grant_decision_logistics_notes */
+    NULL, /* grant_publication_logistics_notes */
+    NULL, /* grant_financing_notes */
+    NULL /* notes */
+  ),
+
   ('Pineapple Fund',NULL,'United States',NULL,'https://pineapplefund.org/','https://pineapplefund.org/',NULL,NULL,NULL,NULL,NULL,'PineappleFund',NULL,NULL,NULL,NULL,'irregular',NULL,'days','months','https://github.com/riceissa/pineapple-fund','https://en.wikipedia.org/wiki/Pineapple_Fund',NULL,
   'The Pineapple Fund was announced in December 2017 by an anonymous individual.',
   NULL, NULL, NULL, NULL, NULL);
