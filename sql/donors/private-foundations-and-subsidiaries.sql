ed015aae2d4d85075027823b3d6d8f7d1445034d
Issa Rice <riceissa@gmail.com>
2019-04-23 18:02:37 +0100

    Add John Merck Fund metadata
    For #96.

--- sql/donors/private-foundations-and-subsidiaries.sql
+++ sql/donors/private-foundations-and-subsidiaries.sql
@@ -240,6 +240,38 @@ insert into donors(donor, donor_type, country, facebook_username, website, donat
     NULL /* notes */
   ),
 
+  (
+    'John Merck Fund', /* donor */
+    'Private foundation', /* donor_type */
+    'United States', /* country */
+    NULL, /* facebook_username */
+    'https://www.jmfund.org/', /* website */
+    'https://www.jmfund.org/program-grants/', /* donations_url */
+    NULL, /* lesswrong_username */
+    NULL, /* linkedin_username */
+    NULL, /* affiliated_orgs */
+    NULL, /* eaf_username */
+    NULL, /* eahub_username */
+    NULL, /* github_username */
+    NULL, /* twitter_username */
+    NULL, /* predictionbook_username */
+    'https://www.jmfund.org/about-us/', /* philosophy_url */
+    'https://www.jmfund.org/for-grantseekers/', /* grant_application_process_url */
+    NULL, /* donations_data_update_regularity */
+    NULL, /* donations_data_dlw_update_regularity */
+    NULL, /* donations_data_update_lag */
+    NULL, /* donations_data_dlw_update_lag */
+    'https://github.com/riceissa/john-merck-fund', /* dlw_data_processing_script */
+    NULL, /* wikipedia_page */
+    NULL, /* best_overview_url */
+    NULL, /* brief_history */
+    NULL, /* brief_donor_focus_notes */
+    NULL, /* grant_decision_logistics_notes */
+    NULL, /* grant_publication_logistics_notes */
+    NULL, /* grant_financing_notes */
+    NULL /* notes */
+  )
+
   ('Pineapple Fund',NULL,'United States',NULL,'https://pineapplefund.org/','https://pineapplefund.org/',NULL,NULL,NULL,NULL,NULL,'PineappleFund',NULL,NULL,NULL,NULL,'irregular',NULL,'days','months','https://github.com/riceissa/pineapple-fund','https://en.wikipedia.org/wiki/Pineapple_Fund',NULL,
   'The Pineapple Fund was announced in December 2017 by an anonymous individual.',
   NULL, NULL, NULL, NULL, NULL);
