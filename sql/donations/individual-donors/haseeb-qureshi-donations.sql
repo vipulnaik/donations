ba40bdc053bff8c4a4708dc9d3b17a4d3564eec9
Vipul Naik <vipulnaik1@gmail.com>
2019-09-09 10:02:46 -0700

    Continue restructuring Haseeb Qureshi grants

--- sql/donations/individual-donors/haseeb-qureshi-donations.sql
+++ sql/donations/individual-donors/haseeb-qureshi-donations.sql
@@ -1,5 +1,4 @@
 insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, notes, payment_modality, match_eligible, goal_amount, influencer) values
-  ('Haseeb Qureshi','Centre for Effective Altruism', 10600, 0.5, '2015-01-01','year','date of donation announced in announcement','Effective altruism/movement growth','http://haseebq.com/my-first-year-earning-to-give-a-retrospective/','Money was for the EA Outreach team run by Kerry Vaughan and Tyler Alterman, that was responsible for running EA Global',NULL,NULL,21200,'GiveWell'),
   ('Haseeb Qureshi', 'Against Malaria Foundation', 12650, 0.25, '2016-01-01','year','date of donation announced in announcement','Global health/malaria','http://haseebq.com/2016-donations-and-some-thoughts/','Highlighted rock-solid work at saving lives and the need to spend on surefire things in addition to speculative investments', NULL, NULL, 50600, 'GiveWell'),
   ('Haseeb Qureshi', 'Rethink Charity',12650,0.25,'2016-01-01','year','date of donation announced in announcement','Effective altruism/movement growth','http://haseebq.com/2016-donations-and-some-thoughts/','Highlighted its projects including the Effective Altruism Forum', NULL, NULL, 50600,NULL),
   ('Haseeb Qureshi', '80,000 Hours',25300, 0.5,'2016-01-01','year','date of donation announced in announcement','Effective altruism/movement growth','http://haseebq.com/2016-donations-and-some-thoughts/','Highlighted impact potential, significant growth, and personal anecdote of how it influenced the author', NULL, NULL, 50600,NULL),
@@ -10,26 +9,38 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   ('Haseeb Qureshi','GiveDirectly', 25000, '2013-01-01', 'year', 'donation log', 'Cash transfers', 'https://eahub.org/user/haseeb-qureshi', 25000, 1.0, 'GiveWell', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 
   ('Haseeb Qureshi','Against Malaria Foundation', 5300, '2015-01-01','year','date of donation announced in announcement','Global health/malaria','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',21200, 0.25, 'GiveWell', '2015-12-31','day','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',
-  /* donation_process */ 'The announcement says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." The allocation of this $21,200 is based on analysis of the individual target charities',
+  /* donation_process */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." The allocation of this $21,200 is based on analysis of the individual target charities',
   /* intended_use_of_funds_category */ 'Organizational general support',
   /* intended_use_of_funds */ 'The donee "distributes low-cost anti-mosquito bednets in parts of Africa that are ravaged by malaria"',
   /* intended_funding_timeframe_in_months */ NULL,
   /* donor_donee_reason */ 'The announcement post says of the donee: "AMF is ranked by Givewell as the most marginally impactful charity in the world."',
-  /* donor_amount_reason */ 'The announcement says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." AMF gets 25% of this money; the amount it gets is likely constrained by the needs of other recipients',
+  /* donor_amount_reason */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." AMF gets 25% of this money; the amount it gets is likely constrained by the needs of other recipients',
   /* donor_timing_reason */ 'End-of-year round of donations of 33% of income in the year; this is part of the donor''s earning-to-give strategy',
   /* donor_next_donation_thoughts */ NULL,
   /* donor_retrospective */ 'The donor would continue to donate to Against Malaria Foundation for the next two years; see https://haseebq.com/2016-donations-and-some-thoughts/ and https://medium.freecodecamp.org/yearly-donations-and-why-you-should-give-now-not-later-cba890cabfbe but would stop donating to it in 2018',
   /* notes */ NULL),
 
   ('Haseeb Qureshi','Rethink Charity', 5300, '2015-01-01','year','date of donation announced in announcement','Effective altruism/movement growth','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',21200, 0.25, 'GiveWell', '2015-12-31','day','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',
-  /* donation_process */ 'The announcement says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." The allocation of this $21,200 is based on analysis of the individual target charities',
+  /* donation_process */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." The allocation of this $21,200 is based on analysis of the individual target charities',
   /* intended_use_of_funds_category */ 'Organizational general support',
   /* intended_use_of_funds */ 'At the time of the donation, the donee is known as .impact (it would later rename itself to Rethink Charity). The announcement post calls it "a grassroots EA organization with which I’ve grown more involved over the last year. .impact is best known for creating a ton of EA infrastructure, including the EA Forums and EA Hub. Their latest project is LEAN, which helps to seed and support EA chapters all around the world, helping to foster the movement at the grassroots level."',
   /* intended_funding_timeframe_in_months */ NULL,
   /* donor_donee_reason */ 'The announcement post says: ".impact is best known for creating a ton of EA infrastructure, including the EA Forums and EA Hub. Their latest project is LEAN, which helps to seed and support EA chapters all around the world, helping to foster the movement at the grassroots level."',
-  /* donor_amount_reason */ 'The announcement says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." Rethink Charity gets 25% of this money; the amount it gets is likely constrained by the needs of other recipients',
+  /* donor_amount_reason */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." Rethink Charity gets 25% of this money; the amount it gets is likely constrained by the needs of other recipients',
   /* donor_timing_reason */ 'End-of-year round of donations of 33% of income in the year; this is part of the donor''s earning-to-give strategy',
   /* donor_next_donation_thoughts */ NULL,
   /* donor_retrospective */ 'The donor would continue to make similar donations in later years: to Rethink Charity in 2016 https://haseebq.com/2016-donations-and-some-thoughts/and to the Local Effective Altruism Network in 2018 https://haseebq.com/2018-donations/',
+  /* notes */ NULL),
+
+  ('Haseeb Qureshi','Centre for Effective Altruism', 10600,'2015-01-01','year','date of donation announced in announcement','Effective altruism/movement growth','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',21200, 0.5, 'GiveWell', '2015-12-31','day','https://haseebq.com/my-first-year-earning-to-give-a-retrospective/',
+  /* donation_process */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." The allocation of this $21,200 is based on analysis of the individual target charities',
+  /* intended_use_of_funds_category */ 'Direct project expenses',
+  /* intended_use_of_funds */ 'Donation for the EA Outreach team run by Kerry Vaughan and Tyler Alterman. The announcement post says: "EA Outreach was responsible for EA Global, the major EA conference this year at Google HQ, and have ambitious plans on how to grow the movement and influence more of the world toward evidence-based giving."'
+  /* intended_funding_timeframe_in_months */ NULL,
+  /* donor_donee_reason */ 'The announcement post says: "[EA Outreach] have ambitious plans on how to grow the movement and influence more of the world toward evidence-based giving. I think the impact they could have is enormous, and so I decided to give them half of my donations this year."',
+  /* donor_amount_reason */ 'The announcement post says: "In all I gave 33% of my pre-tax income, which since I started my job in June, amounts to $21,200 USD I donated to high-impact charities." EA Outreach gets 50% of this money; the amount it gets is likely constrained by the needs of other recipients',
+  /* donor_timing_reason */ 'End-of-year round of donations of 33% of income in the year; this is part of the donor''s earning-to-give strategy',
+  /* donor_next_donation_thoughts */ NULL,
+  /* donor_retrospective */ NULL,
   /* notes */ NULL);
 
