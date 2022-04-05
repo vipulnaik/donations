bc474487d1df5614cfa9bf83261cf6e497f00684
Vipul Naik <vipulnaik1@gmail.com>
2022-04-04 18:28:42 -0700

    Add missing files

---
+++ sql/donations/individual-donors/howie-lempel-donations.sql
@@ -0,0 +1,6 @@
+insert into donations(donor, donee, amount, fraction, donation_date, donation_date_precision, donation_date_basis, cause_area, url, influencer, donor_donee_reason, donor_amount_reason, donor_timing_reason, notes) values
+  ('Howie Lempel','Carrick Flynn for Congress',5800,NULL,'2022-02-03','day','date of donation announcement','Biosecurity and pandemic preparedness','https://forum.effectivealtruism.org/posts/Qi9nnrmjwNbBqWbNT/the-best-usd5-800-i-ve-ever-donated-to-pandemic-prevention?commentId=rZMS6CmLZwD9J8Cah',NULL,
+  /* donor_donee_reason */ 'The announcement of the donation by Howie Lempel is on Andrew Snyder-Beattie''s post https://forum.effectivealtruism.org/posts/Qi9nnrmjwNbBqWbNT/the-best-usd5-800-i-ve-ever-donated-to-pandemic-prevention advocating that people donate to the cause. The comment says: "I also donated $5,800 (though not due to this post)."',
+  /* donor_amount_reason */ 'Donations to United States political campaigns are limited to $2,900 per donor per campaign. The limit applies separately to the primary and the general election, resulting in an effective limit of $5,800.',
+  /* donor_timing_reason */ 'The timing is determined by the timing of https://www.carrickflynnfororegon.com/ (the political campaign).',
+  /* notes */ NULL);
