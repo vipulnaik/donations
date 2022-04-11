8aa746416beb7406517744b7a3879958b815b3b6
Issa Rice <riceissa@gmail.com>
2022-04-10 19:03:32 -0700

    Add theme changer (#140)
    * Add theme changer
    
    * Use better href and wrap theme changer in blank paragraph
    
    * Try to force blank paragraph using unicode thin space

--- access-portal/donee.php
+++ access-portal/donee.php
@@ -1,5 +1,6 @@
 <?php
 include_once('doctype.inc');
+print '<script src="change-theme.js"></script>';
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/lists.inc");
 $donee = 'Vipul Naik';
@@ -31,6 +32,7 @@ include_once('strip-commas.inc');
 include_once('backend/stringFunctions.inc');
 include_once('backend/cachingFunctions.inc');
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
+include_once("style.inc");
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
 print '</head>';
@@ -45,6 +47,7 @@ print '<script>$(document).ready(function()
     }
 ); </script>'."\n";
 print "<h3>$donee donations received</h3>";
+print '<p><span id="changeThemeMenu" style="display: none;">Change <a href="javascript:;" onclick="change_theme_color()">color</a></span>   &thinsp;<!-- blank space to prevent cumulative layout shift --></p>';
 include_once('preamble.inc');
 print '<h4>Table of contents</h4>';
 print '<ul>';
