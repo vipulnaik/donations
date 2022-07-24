8212111dbd455327ba9004175d842a5053d5a6e1
Vipul Naik <vipulnaik1@gmail.com>
2022-07-24 10:08:20 -0700

    Start adding FTX Future Fund grants

---
+++ sql/donations/private-foundations/ftx-future-fund/ftx-future-fund-biosecurity-open-call-grants.sql
@@ -0,0 +1,27 @@
+insert into donations(donor, donee, amount, donation_date, donation_date_precision, donation_date_basis, cause_area, url, donor_cause_area_url, donation_earmark, influencer, donation_announcement_date, donation_announcement_date_precision, donation_announcement_url, predictions, donation_process, intended_use_of_funds_category, intended_use_of_funds, intended_funding_timeframe_in_months, donor_donee_reason, donor_amount_reason, donor_timing_reason, donor_next_donation_thoughts, donor_retrospective, notes) values
+  ('FTX Future Fund','SecureBio',1200000,'2022-03-01','month','donation log','Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/','https://ftxfuturefund.org/area-of-interest/#extinction-and-catastrophe-biorisk-and-recovery-from-catastrophe','Kevin Esvelt',NULL,NULL,NULL,NULL,NULL,
+  /* donation_process */ 'According to https://forum.effectivealtruism.org/posts/paMYXYFYbbjpdjgbt/future-fund-june-2022-update#Open_call this grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
+  /* intended_use_of_funds_category */ 'Direct project expenses',
+  /* intended_use_of_funds */ 'The grant description states: "We recommended a grant to support the hiring of several key staff for Dr. Kevin Esvelt’s pandemic prevention work. SecureBio is working to implement universal DNA synthesis screening, build a reliable early warning system, and coordinate the development of improved personal protective equipment and its delivery to essential workers when needed."',
+  /* intended_funding_timeframe_in_months */ NULL,
+  /* donor_donee_reason */ NULL,
+  /* donor_amount_reason */ NULL,
+  /* donor_timing_reason */ 'Timing determined by timing of the open call https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 for applications; the grant is made close to the application window for the open call (2022-02-28 to 2022-03-21).',
+  /* donor_next_donation_thoughts */ NULL,
+  /* donor_retrospective */ NULL,
+  /* notes */ 'The June 2022 update https://ftxfuturefund.org/future-fund-june-2022-update/ by the FTX Future Fund highlights the grant as one of its example grants.'),
+
+  ('FTX Future Fund','Institute for Progress',480000,'2022-05-01','month','donation log','Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/','https://ftxfuturefund.org/area-of-interest/#extinction-and-catastrophe-biorisk-and-recovery-from-catastrophe',NULL,NULL,NULL,NULL,NULL,NULL,
+  /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
+  /* intended_use_of_funds_category */ 'Direct project expenses',
+  /* intended_use_of_funds */ 'The grant description says: "We recommended a grant to support the Institute’s research and policy engagement work on high skilled immigration, biosecurity, and pandemic prevention."',
+  /* intended_funding_timeframe_in_months */ NULL,
+  /* donor_donee_reason */ NULL,
+  /* donor_amount_reason */ NULL,
+  /* donor_timing_reason */ 'Timing determined by timing of the open call https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 for applications; the grant is made close to the application window for the open call (2022-02-28 to 2022-03-21).',
+  /* donor_next_donation_thoughts */ NULL,
+  /* donor_retrospective */ NULL,
+  /* notes */ NULL);
+
+  /* Grant to ALTER is in AI safety grants file */
+  /* Many more grants to be added */
