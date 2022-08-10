e48ce540747e38f97d8c187f8c2fbce9fd8acb44
Vipul Naik <vipulnaik1@gmail.com>
2022-08-10 13:55:54 -0700

    Add more FTX grants (not fully vetted)

--- sql/donations/private-foundations/ftx-future-fund/ftx-future-fund-ai-safety-open-call-grants.sql
+++ sql/donations/private-foundations/ftx-future-fund/ftx-future-fund-ai-safety-open-call-grants.sql
@@ -1,5 +1,5 @@
 insert into donations(donor, donee, amount, donation_date, donation_date_precision, donation_date_basis, cause_area, url, donor_cause_area_url, donation_earmark, influencer, donation_announcement_date, donation_announcement_date_precision, donation_announcement_url, predictions, donation_process, intended_use_of_funds_category, intended_use_of_funds, intended_funding_timeframe_in_months, donor_donee_reason, donor_amount_reason, donor_timing_reason, donor_next_donation_thoughts, donor_retrospective, notes) values
-  ('FTX Future Fund','Siddharth Hiregowdara',100000,'2022-03-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Siddharth Hiregowdara',100000,'2022-03-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support the production of high quality materials for learning about AI safety work."',
@@ -11,7 +11,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','Cornell University',1500000,'2022-04-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Lionel Levine',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Cornell University',1500000,'2022-04-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Lionel Levine',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support Prof. Levine, as well as students and collaborators, to work on alignment theory research at the Cornell math department."',
@@ -23,7 +23,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ 'The June 2022 update https://ftxfuturefund.org/future-fund-june-2022-update/ by the FTX Future Fund highlights the grant as one of its example grants.'),
 
-  ('FTX Future Fund','Columbia University',100000,'2022-04-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Claudia Shi',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Columbia University',100000,'2022-04-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Claudia Shi',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support the work of a PhD student [Claudia Shi] working on AI safety at Columbia University."',
@@ -35,7 +35,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','Prometheus Science Bowl',100000,'2022-05-01','month','donation log','AI safety/talent pipeline','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Prometheus Science Bowl',100000,'2022-05-01','month','donation log','AI safety/talent pipeline','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support a competition for work on Eliciting Latent Knowledge, an open problem in AI alignment, for talented high school and college students who are participating in Prometheus Science Bowl."',
@@ -47,7 +47,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','University of California, Berkeley',800000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Anca Dragan',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','University of California, Berkeley',800000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Anca Dragan',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support a project to develop interactive AI algorithms for alignment that can uncover the causal features in human reward systems, and thereby help AI systems learn underlying human values that generalize to new situations."',
@@ -59,7 +59,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','Association for Long Term Existence and Resilience',320000,'2022-05-01','month','donation log','AI safety|Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Association for Long Term Existence and Resilience',320000,'2022-05-01','month','donation log','AI safety|Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support ALTER, an academic research and advocacy organization, which hopes to investigate, demonstrate, and foster useful ways to improve the future in the short term, and to safeguard and improve the long-term trajectory of humanity. The organization''s initial focus is building bridges to academia via conferences and grants to find researchers who can focus on AI safety, and on policy for reducing biorisk."',
@@ -71,7 +71,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','University of Cambridge',380000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Gabriel Recchia',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','University of Cambridge',380000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Gabriel Recchia',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support research on how to fine-tune GPT-3 models to identify flaws in other fine-tuned language models'' arguments for the correctness of their outputs, and to test whether these help nonexpert humans successfully judge such arguments."',
@@ -83,7 +83,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','AI Safety Support',200000,'2022-05-01','month','donation log','AI safety/talent pipeline','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','AI Safety Support',200000,'2022-05-01','month','donation log','AI safety/talent pipeline','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant "for general funding for community building and managing the talent pipeline for AI alignment researchers. AI Safety Support’s work includes one-on-one coaching, events, and research training programs."',
@@ -95,7 +95,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','University of Utah',280000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Daniel Brown',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','University of Utah',280000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Daniel Brown',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support research on value alignment in AI systems, practical algorithms for efficient value alignment verification, and user studies and experiments to test these algorithms."',
@@ -107,7 +107,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','Brian Christian',300000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','Brian Christian',300000,'2022-05-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support the completion of a book which explores the nature of human values and the implications for aligning AI with human preferences."',
@@ -119,7 +119,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','James Lin',190000,'2022-05-01','month','donation log','AI safety|Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','James Lin',190000,'2022-05-01','month','donation log','AI safety|Biosecurity and pandemic preparedness','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "allow a reputable technology publication to engage 2-5 undergraduate student interns to write about topics including AI safety, alternative proteins, and biosecurity."',
@@ -131,7 +131,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','AI Safety Camp',290000,'2022-06-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','AI Safety Camp',290000,'2022-06-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence',NULL,NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "partially support the salaries for AI Safety Camp’s two directors and to support logistical expenses at its physical camp."',
@@ -143,7 +143,7 @@ insert into donations(donor, donee, amount, donation_date, donation_date_precisi
   /* donor_retrospective */ NULL,
   /* notes */ NULL),
 
-  ('FTX Future Fund','University of California, Berkeley',600000,'2022-06-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/#artificial-intelligence','Sergey Levine',NULL,NULL,NULL,NULL,NULL,
+  ('FTX Future Fund','University of California, Berkeley',600000,'2022-06-01','month','donation log','AI safety','https://ftxfuturefund.org/our-grants/?_funding_stream=open-call','https://ftxfuturefund.org/area-of-interest/artificial-intelligence','Sergey Levine',NULL,NULL,NULL,NULL,NULL,
   /* donation_process */ 'This grant is a result of the Future Fund''s open call for applications originally announced on 2022-02-28 at https://forum.effectivealtruism.org/posts/2mx6xrDrwiEKzfgks/announcing-the-future-fund-1 with a deadline of 2022-03-21.',
   /* intended_use_of_funds_category */ 'Direct project expenses',
   /* intended_use_of_funds */ 'Grant to "support a project to study how large language models integrated with offline reinforcement learning pose a risk of machine deception and persuasion."',
