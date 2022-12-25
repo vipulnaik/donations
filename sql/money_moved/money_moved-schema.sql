503c73192b2371bb580df96fb73c2fc117e52085
Vipul Naik <vipulnaik1@gmail.com>
2022-12-24 21:20:44 -0800

    Fix charset and address other mysql warnings; try utf8mb3 to deal with row size issue

--- sql/money_moved/money_moved-schema.sql
+++ sql/money_moved/money_moved-schema.sql
@@ -3,10 +3,10 @@ create table money_moved(
   influencer varchar(100),
   identified_donees varchar(200),
   identified_donors varchar(200),
-  amount float(14,2),
+  amount decimal(14,2),
   start_date date,
   end_date date,
   document_url varchar(200),
   document_title varchar(200),
   notes varchar(1000)
-)  ENGINE=InnoDB AUTO_INCREMENT=15239276 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+)  ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
